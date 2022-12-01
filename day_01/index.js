cb6364dc189006c8c9d5abf1a670306b08a955bd
Maksim Shubat <shubat.maksim.external@atos.net>
2022-12-01 17:56:26 +0300

    add puzzle description

--- day_01/index.js
+++ day_01/index.js
@@ -4,18 +4,18 @@ const readFile = async (filename) => {
   return await fs.readFile(filename, 'utf-8')
 }
 
-/* Straight through approach */
+/* Part 1 - Straight through approach */
 const getMaxCarryingWeight = (weightData) => {
   return Math.max(...weightData.split(/\r\n\r\n/).map(s => s.split(/\r\n/).reduce((a, s) => a += Number.parseInt(s), 0)))
 }
 
-/* Straight through approach via sort */
+/* Part 2 - Straight through approach via sort */
 const getTop3CarryingWeightSort = (weightData) => {
   const sortedWeights = weightData.split(/\r\n\r\n/).map(s => s.split(/\r\n/).reduce((a, s) => a += Number.parseInt(s), 0)).sort((a, b) =>  a > b ? -1 : a < b ? 1 : 0)
   return sortedWeights[0] + sortedWeights[1] + sortedWeights[2]
 }
 
-/* Dynamic programming approach */
+/* Part 2 - Dynamic programming approach */
 const getTop3CarryingWeightDP = (weightData) => {
   const strings = weightData.split(/\r\n/)
   strings.push('') // allow last iteration to finish
@@ -25,7 +25,7 @@ const getTop3CarryingWeightDP = (weightData) => {
     if(num){
       sums[0] += num
     } else {
-      // todo: rethink the solution
+      // TODO: rethink the solution
       //  problem: [73, 71, 70, 80]
       //  => 73 will replace 71 (second lowest)
       sums.sort((a, b) =>  a > b ? 1 : a < b ? -1 : 0)
@@ -53,4 +53,64 @@ const getTop3CarryingWeightDP = (weightData) => {
   const top3CarryingSort = getTop3CarryingWeightSort(weightData)
   console.log(top3CarryingSort)
 })()
+
+
+// --- Day 1: Calorie Counting ---
+// Santa's reindeer typically eat regular reindeer food, but they need a lot of magical energy to deliver presents on Christmas.
+// For that, their favorite snack is a special type of star fruit that only grows deep in the jungle.
+// The Elves have brought you on their annual expedition to the grove where the fruit grows.
+//
+// To supply enough magical energy, the expedition needs to retrieve a minimum of fifty stars by December 25th.
+// Although the Elves assure you that the grove has plenty of fruit, you decide to grab any fruit you see along the way, just in case.
+//
+// The jungle must be too overgrown and difficult to navigate in vehicles or access from the air; the Elves' expedition traditionally goes on foot.
+// As your boats approach land, the Elves begin taking inventory of their supplies.
+// One important consideration is food - in particular, the number of Calories each Elf is carrying (your puzzle input).
+//
+// The Elves take turns writing down the number of Calories contained by the various meals, snacks, rations, etc. that they've brought with them, one item per line.
+// Each Elf separates their own inventory from the previous Elf's inventory (if any) by a blank line.
+//
+// For example, suppose the Elves finish writing their items' Calories and end up with the following list:
+//
+// 1000
+// 2000
+// 3000
+//
+// 4000
+//
+// 5000
+// 6000
+//
+// 7000
+// 8000
+// 9000
+//
+// 10000
+//
+// This list represents the Calories of the food carried by five Elves:
+//
+// The first Elf is carrying food with 1000, 2000, and 3000 Calories, a total of 6000 Calories.
+// The second Elf is carrying one food item with 4000 Calories.
+// The third Elf is carrying food with 5000 and 6000 Calories, a total of 11000 Calories.
+// The fourth Elf is carrying food with 7000, 8000, and 9000 Calories, a total of 24000 Calories.
+// The fifth Elf is carrying one food item with 10000 Calories.
+// In case the Elves get hungry and need extra snacks, they need to know which Elf to ask:
+// they'd like to know how many Calories are being carried by the Elf carrying the most Calories.
+// In the example above, this is 24000 (carried by the fourth Elf).
+//
+// Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+//
+// --- Part Two ---
+// By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the most Calories of food might eventually run out of snacks.
+//
+// To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried by the top three Elves carrying the most Calories.
+// That way, even if one of those Elves runs out of snacks, they still have two backups.
+//
+// In the example above, the top three Elves are the fourth Elf (with 24000 Calories),
+// then the third Elf (with 11000 Calories),
+// then the fifth Elf (with 10000 Calories).
+// The sum of the Calories carried by these three elves is 45000.
+//
+// Find the top three Elves carrying the most Calories.
+// How many Calories are those Elves carrying in total?
 
