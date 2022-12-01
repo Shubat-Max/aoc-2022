1d12be27c704a4611cdb6eed949ec66e4d869d85
Maksim Shubat <shubat.maksim.external@atos.net>
2022-12-01 17:40:52 +0300

    add solution to AOC day 01 - part 1: max weight & part 2: sum of top 3 max weights calculation

---
+++ day_01/index.ts
@@ -0,0 +1,61 @@
+const fs = require('fs/promises')
+
+const readFile = async (filename) => {
+  return await fs.readFile(filename, 'utf-8')
+}
+
+/* Straight through approach */
+const getMaxCarryingWeight = (weightData) => {
+  return Math.max(...weightData.split(/\r\n\r\n/).map(s => s.split(/\r\n/).reduce((a, s) => a += Number.parseInt(s), 0)))
+}
+
+/* Straight through approach via sort */
+const getTop3CarryingWeightSort = (weightData) => {
+  const sortedWeights = weightData.split(/\r\n\r\n/).map(s => s.split(/\r\n/).reduce((a, s) => a += Number.parseInt(s), 0)).sort((a, b) =>  a > b ? -1 : a < b ? 1 : 0)
+  return sortedWeights[0] + sortedWeights[1] + sortedWeights[2]
+}
+
+/* Dynamic programming approach */
+const getTop3CarryingWeight = (weightData) => {
+  const strings = weightData.split(/\r\n/)
+  strings.push('') // allow last iteration to finish
+  const sums = [0,0,0,0]
+  strings.forEach(s => {
+    const num = Number.parseInt(s)
+    if(num){
+      sums[0] += num
+    } else {
+      // todo: rethink the solution
+      //  problem: [73, 71, 70, 80]
+      //  => 73 will replace 71 (second lowest)
+      sums.sort((a, b) =>  a > b ? 1 : a < b ? -1 : 0)
+      if(sums[0] > sums[3]) sums[3] = sums[0]
+      else if(sums[0] > sums[2]) sums[2] = sums[0]
+      else if(sums[0] > sums[1]) sums[1] = sums[0]
+      sums[0] = 0
+    }
+  })
+
+  return sums.reduce((t, s) => t += s, 0)
+}
+
+
+
+(async () => {
+  const weightData = await readFile('input.txt')
+
+  // const maxCarrying = getMaxCarryingWeight(weightData)
+  // console.log(maxCarrying)
+  //
+  console.time('getTop3CarryingWeight()')
+  const top3Carrying = getTop3CarryingWeight(weightData)
+  console.timeEnd('getTop3CarryingWeight()')
+  console.log(top3Carrying)
+
+  console.time('getTop3CarryingWeightSort()')
+  const top3CarryingSort = getTop3CarryingWeightSort(weightData)
+  console.timeEnd('getTop3CarryingWeightSort()')
+  console.log(top3CarryingSort)
+
+})()
+
